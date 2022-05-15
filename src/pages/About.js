063031c5b0fdbc2e2de4bcf7c23acb25045d05fe
Vaibhav Tyagi <tyagivaibhav56@gmail.com>
2022-05-15 20:53:24 +0530

    cocktail done

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className='section about-section'>
+     <h1 className='section-title'>about us</h1>
+     <p>This is a cocktail page and we do have different types of cocktail available for you.
+       Let's have some fun and enjoy the flavour of our cocktails. Just for you !!!!
+     </p>
+   </section>
+  )
+}
+
+export default About
